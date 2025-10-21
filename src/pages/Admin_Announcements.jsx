985b01eb03c707ac4b2db94e7f0469529028db85
DreiFinity <vladimaizumi@gmail.com>
2025-10-21 20:11:55 +0800

    fix transparent bg of announcement edit modal

--- src/pages/Admin_Announcements.jsx
+++ src/pages/Admin_Announcements.jsx
@@ -446,7 +446,7 @@ const Admin_Announcements = () => {
 
       {/* Edit Modal */}
       {editModal && editData && (
-        <div className="fixed inset-0 bg-black/40 bg-opacity-40 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-black/40  flex items-center justify-center z-50">
           <div className="bg-white rounded-lg p-6 w-full max-w-lg">
             <h2 className="text-xl font-bold mb-4">Edit Announcement</h2>
             {/* Title */}
